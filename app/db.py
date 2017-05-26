41889598270179ae116f2dea8e120b41a721f995
Chris Millet @ChristophersMBP <chris@thelazycompany.com>
2017-05-26 14:00:48 -0400

    added db connection for heroku instance

--- app/db.py
+++ app/db.py
@@ -1,8 +1,14 @@
 import configparser
+import os
+from pymongo import MongoClient
 
 config = configparser.ConfigParser()
 config.read('app/config.ini')
 
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
+if os.environ.get("HEROKU"):
+    uri = os.environ.get("MONGODB_URI")
+    client = MongoClient(uri)
+else:
+    client = MongoClient('localhost', 27017)
+
 db = client.ccdaExamples
