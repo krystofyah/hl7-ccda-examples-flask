43131a4df59645718c76cefa7be68fab242ee832
Chris Millet @ChristophersMBP <chris@thelazycompany.com>
2017-05-26 14:22:48 -0400

    updating host/port

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from app import application
-#from app import socketio as application
 import os
 
 ROOTDIR = os.path.abspath(os.path.dirname(__file__))
@@ -7,13 +6,12 @@ ROOTDIR = os.path.abspath(os.path.dirname(__file__))
 application.jinja_loader.searchpath=[os.path.normpath(os.path.join(ROOTDIR,'templates'))]
 application.static_folder=os.path.normpath(os.path.join(ROOTDIR,'static'))
 import os
-if 'IP' in os.environ and 'PORT' in os.environ:
-    host = os.environ['IP']
-    port = int(os.environ['PORT'])
+if 'APP_URI' in os.environ and 'APP_PORT' in os.environ:
+    host = os.environ['APP_URI']
+    port = int(os.environ['APP_PORT'])
 else:
     host = '127.0.0.1'
     port = 7000
 
 if __name__ == "__main__":
     application.run(host=host, port=port, debug=True)
-    #socketio.run()
