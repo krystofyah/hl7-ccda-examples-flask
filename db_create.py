6dceef3e9a294cb9e3435766ddc49913b218b8aa
Chris Millet @ChristophersMBP <chris@thelazycompany.com>
2017-05-26 23:06:20 -0400

    added script for loading existing examples

--- db_create.py
+++ db_create.py
@@ -1,8 +1,20 @@
 #!flask/bin/python
 from app.db import db
 import os.path
+import git
+import ipdb
+import git
+import shutil
+from parse_meta_data import parse
 
+LOCAL_EXAMPLES_REPO_DIR = "./ccda_examples_repo"
+
+repo = git.Repo.clone_from("https://github.com/schmoney/C-CDA-Examples.git", LOCAL_EXAMPLES_REPO_DIR)
+repo.git.pull("origin", "master")
+parse(LOCAL_EXAMPLES_REPO_DIR)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-results = db.examples.find()
-print results 
+shutil.rmtree(LOCAL_EXAMPLES_REPO_DIR)
+sections = db.sections.find().count()
+examples = db.examples.find().count()
+print "loaded {} sections and {} examples".format(sections, examples)
---
+++ parse_meta_data.py
@@ -0,0 +1,119 @@
+import os, ipdb, re, pandoc
+
+folder = '../C-CDA-Examples'
+
+import git
+repo = git.Repo(folder)
+VALIDATOR_LOOKUP = {
+    "https://sitenv.org/c-cda-validator": "SITE C-CDA Validator"
+}
+
+from app.db import db
+
+def get_name(section):
+    lines = section.split("\n\n")
+    name = lines[0].strip()
+    try:
+        colon = name.index(':')
+        name = name[:colon]
+    except:
+        pass
+    return name
+
+
+def process_section(doc, name, section):
+    #name_criteria = name if name == 'Approval Status' else "#{}".format(name)
+    if section.startswith(name): # not clear what this section maps to in the front end
+        lines = section.split("\n")
+        #   ipdb.set_trace()
+        lines = [ l.replace('* ', '') for l in lines if l.startswith("*") ]
+        #   get rid of remaining #
+        name = name[1:] if name.startswith("#") else name
+        # make name safe for mongo
+        name = name.replace(".", ' ')
+
+        #   update Permalink field to just be the id, otherwise list of bulleted items
+        isStr = name in ['Permalink', 'Comments', 'Custodian', 'Reference to full CDA sample']
+        if name == 'Approval Status':
+            doc['approval']  = lines[0].split(":")[1].strip()
+        if name == 'Validation location':
+            link = lines[0][lines[0].index('(')+1 : lines[0].index(')')]
+            doc['validator'] = {
+                                "link": link,
+                                "name": VALIDATOR_LOOKUP[link]
+                                }
+        doc[name] = lines[0] if isStr else lines
+
+
+def process_sections(sections):
+    doc = {}
+    for section in sections:
+        if section != '':
+            name = get_name(section)
+            process_section(doc,name, section)
+
+            #   ipdb.set_trace()
+            """
+            if section.startswith("Approval Status"):
+                lines = section.split("\n")
+                lines = [ l for l in lines if l.startswith("*")]
+                doc['Approval Status'] = lines
+            if section.startswith("#C-CDA 2.1 Example"): # not clear what this section maps to in the front end
+                lines = section.split("\n")
+                lines = [ l for l in lines if l.startswith("*")]
+                doc['C-CDA 2.1 Example'] = lines
+            """
+    return doc
+
+
+def process_readme(section_name, example_name, data):
+    sections = data.split('##')
+    doc = process_sections(sections)
+    doc['section'] = section_name
+    doc['name'] = example_name
+    if 'Permalink' in doc:
+        result = db.examples.replace_one({"Permalink": doc['Permalink']}, doc, upsert=True)
+        #   ipdb.set_trace()
+    else:
+        #   add permalink to readme
+        pass
+        #   commit change to readme
+
+        #   push change to GitHub repo
+
+
+#   loop through each section folder
+def parse(folder):
+    for path,dirs,files in os.walk(folder):
+        #   print "path: {} dir: {} "
+        dirs[:] = [d for d in dirs if not d[0] == '.']
+        for filename in files:
+            #   section folder
+            if filename.lower() == "readme.md" and len(dirs) != 0:
+                print os.path.join(path,filename)
+                pth = os.path.join(re.sub(folder, '', path), filename)
+                pth = pth.lstrip('/')
+                with open(os.path.join(path,filename), 'rb') as readme:
+                    description = readme.read()
+                    description = description.replace("##", "")
+                    section_name = path.split(os.path.sep)[-1]
+                    #   ipdb.set_trace()
+                    if section_name != 'C-CDA-Examples':
+                        db.sections.replace_one(
+                            {"name": section_name},
+                            {
+                                "name": section_name,
+                                "description": description
+                            },
+                            upsert=True
+                        )
+
+            #   actual example folder
+            if filename.lower() == "readme.md" and dirs == []:
+                print os.path.join(path,filename)
+                pth = os.path.join(re.sub(folder, '', path), filename)
+                pth = pth.lstrip('/')
+                example_name = path.split(os.path.sep)[-1]
+                with open(os.path.join(path,filename), 'rU') as readme:
+                    data = readme.read()
+                    process_readme(section_name, example_name, data)
